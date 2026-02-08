60fe609f0ed9a06f0f94789093d4a72d63fef8ef
vadym <vadym.ruchka@gmail.com>
2026-02-08 20:53:47 +0100

    Cleanup default files and add plain components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { observer } from 'mobx-react-lite'
-import reactLogo from './assets/react.svg'
-import { counterStore } from './stores/CounterStore'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Tagline } from "./tagline/Tagline.tsx";
 
-const App = observer(() => {
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
-      </div>
-      <h1>Vite + React + MobX</h1>
-      <div className="card">
-        <button onClick={counterStore.increment}>
-          count is {counterStore.count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-})
-
-export default App
+    <div>
+      <Tagline />
+    </div>
+  );
+}
---
+++ src/tagline/Tagline.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const Tagline: FC = () => {
+  return (
+    <div>
+      Tagline
+    </div>
+  );
+}
