496185ba1f6558934e3c5227a65bc6d619c004f0
vadym <vadym.ruchka@gmail.com>
2026-02-09 13:06:55 +0100

    Styles for second dialog

--- src/tagline/dialogs/item/ItemDialog.tsx
+++ src/tagline/dialogs/item/ItemDialog.tsx
@@ -2,6 +2,7 @@ import type { FC } from "react";
 import { DialogBody, DialogHeader } from "../common";
 import { itemDialogStore } from './main-dialog.store.ts';
 import { observer } from "mobx-react-lite";
+import styles from "./item-dialog.module.scss";
 
 export const ItemDialog: FC = observer(() => {
   if (!itemDialogStore.isVisible) {
@@ -11,6 +12,22 @@ export const ItemDialog: FC = observer(() => {
   return (
     <DialogBody>
       <DialogHeader label={'Item'} onClose={itemDialogStore.close}/>
+      <div className={styles.item__inputs}>
+        <div className={styles.item__inputblock}>
+          <div className={styles.item__label}>Label</div>
+          <input
+            type="text"
+            className={styles.item__input}
+          />
+        </div>
+        <div className={styles.item__inputblock}>
+          <div className={styles.item__label}>Link</div>
+          <input
+            type="text"
+            className={styles.item__input}
+          />
+        </div>
+      </div>
     </DialogBody>
   )
 });
